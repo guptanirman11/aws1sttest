837ec28e6759450e3ee3f2af6191afb843e40122
guptanirman11 <anshulgupta@Anshuls-MacBook-Air.local>
2023-06-26 14:36:01 -0500

    removed mysqli

--- write_data.php
+++ write_data.php
@@ -31,29 +31,7 @@ try{
     //     ltrim($db["path"], "/")
     // ));
 
-    // $dbhost = $_SERVER['RDS_HOSTNAME'];
-    // $dbport = $_SERVER['RDS_PORT'];
-    // $dbname = $_SERVER['RDS_DB_NAME'];
-    // // $charset = 'utf8' ;
-
-    // $dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname}";
-    // $username = $_SERVER['RDS_USERNAME'];
-    // $password = $_SERVER['RDS_PASSWORD'];
-
-    // $options = [
-    //     // PDO::PGSQL_ATTR_AUTHENTICATION => PDO::PGSQL_AUTH_MD5
-    //     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    //     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    //     PDO::ATTR_EMULATE_PREPARES => false,
-    //     PDO::ATTR_PERSISTENT => true,
-    //     PDO::ATTR_STRINGIFY_FETCHES => false
-    // ];
-    // $pdo = new PDO($dsn, $username, $password);
-    // $link = new mysqli($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
-
-    // $pdo = new PDO($dsn, $username, $password);
-
-    $db_host = $_SERVER("RDS_HOSTNAME");
+    $db_host = $_SERVER"RDS_HOSTNAME";
     $db_port = $_SERVER("RDS_PORT");
     $db_name = $_SERVER("RDS_DB_NAME");
     $db_user = $_SERVER("RDS_USERNAME");
