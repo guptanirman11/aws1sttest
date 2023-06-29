db5cff4bbf5ccc3175980b41b2503e016d69ea16
guptanirman11 <anshulgupta@Anshuls-MacBook-Air.local>
2023-06-29 17:30:38 -0500

    added tables

--- write_data.php
+++ write_data.php
@@ -20,16 +20,16 @@ try{
     // Creating the connection between aws rds instance in order to add the responses using AJAX Call.
     $pdo = new mysqli($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
 
-    $pdo->query("DROP TABLE IF EXISTS timeofaction");
-    $pdo->query("DROP TABLE IF EXISTS reaction_time");
-    $pdo->query("DROP TABLE IF EXISTS response");
-    $pdo->query("DROP TABLE IF EXISTS ordering");
+    // $pdo->query("DROP TABLE IF EXISTS timeofaction");
+    // $pdo->query("DROP TABLE IF EXISTS reaction_time");
+    // $pdo->query("DROP TABLE IF EXISTS response");
+    // $pdo->query("DROP TABLE IF EXISTS ordering");
 
     // Create the required tables if they do not exist
-    // $pdo->query("CREATE TABLE IF NOT EXISTS timeofaction (pid TEXT UNIQUE)");
-    // $pdo->query("CREATE TABLE IF NOT EXISTS reaction_time (pid TEXT UNIQUE)");
-    // $pdo->query("CREATE TABLE IF NOT EXISTS response (pid TEXT UNIQUE)");
-    // $pdo->query("CREATE TABLE IF NOT EXISTS ordering (pid TEXT UNIQUE)");
+    $pdo->query("CREATE TABLE IF NOT EXISTS timeofaction (pid TEXT UNIQUE)");
+    $pdo->query("CREATE TABLE IF NOT EXISTS reaction_time (pid TEXT UNIQUE)");
+    $pdo->query("CREATE TABLE IF NOT EXISTS response (pid TEXT UNIQUE)");
+    $pdo->query("CREATE TABLE IF NOT EXISTS ordering (pid TEXT UNIQUE)");
     
     // Dummy value to check if the connection is perfect or not 
    
