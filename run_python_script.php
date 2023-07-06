6e09c7ca3181e0580e041c604d22b5ab500123f5
guptanirman11 <anshulgupta@Anshuls-MacBook-Air.local>
2023-07-06 12:38:41 -0500

    Updated php script

--- run_python_script.php
+++ run_python_script.php
@@ -1,3 +1,8 @@
 <?php
-exec('python3 /bin/generate_data.py');
+try {
+    exec('python3 bin/generate_data.py');
+} catch (Exception $e) {
+    // Handle the exception here
+    error_log('An error occurred: ' . $e->getMessage());
+}
 ?>
